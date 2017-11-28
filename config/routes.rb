a6654f3f10bbc357f18cf197bb7233872d84d111
tvdeyken <code@vandereyken.be>
2017-11-28 16:04:52 +0100

    authentication methods

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,10 @@ get 'about', to: 'welcome#about'
 resources :articles # added
 
 get 'signup', to: 'users#new'
-resources :users, except: [:new] 
+resources :users, except: [:new]
+
+get 'login', to: 'sessions#new'
+post 'login', to: 'sessions#create'
+delete 'logout', to: 'sessions#destroy'
 
 end
